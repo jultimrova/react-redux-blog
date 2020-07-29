dd7fc127382ccd18ae6fb8197f6be32985fb2835
arvein <moonflower019@gmail.com>
2020-07-29 12:06:32 +0300

    add reducers and actions

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers'
 
 ReactDOM.render(
-  <Provider store={createStore(() => {})}>
+  <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
   document.getElementById('root')
