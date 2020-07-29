d308bc75077a3bcb7b4e5744f5a9286606d74418
arvein <moonflower019@gmail.com>
2020-07-29 12:36:31 +0300

    add react router, create posts id component

---
+++ my-blog/src/components/postsId.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class PostsId extends Component {
+    render() {
+        return (
+            <div>Posts Id</div>
+        )
+    }
+}
+
+export default PostsId
--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,19 +1,25 @@
-import './index.css';import React from 'react';
+import './index.css';
+import React from 'react';
 import * as serviceWorker from './serviceWorker';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
 
-import App from './App';
 import reducers from './reducers'
+import {BrowserRouter, Route} from "react-router-dom";
+import PostsId from "./components/postsId";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route path='/' component={PostsId} />
+            </div>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
