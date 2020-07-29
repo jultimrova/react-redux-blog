a9dee83322bd62b1af5c01116d5a2bb154b6f064
arvein <moonflower019@gmail.com>
2020-07-29 13:17:04 +0300

    complete rendering posts

--- my-blog/src/components/postsId.js
+++ my-blog/src/components/postsId.js
@@ -1,11 +1,37 @@
 import React, {Component} from "react";
+import _ from 'lodash'
+import {fetchPosts} from "../actions";
+import {connect} from "react-redux";
 
 class PostsId extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return _.map(this.props.posts, p => {
+            return (
+                <li className='list-group-item' key={p.id}>
+                    {p.title}
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
-            <div>Posts Id</div>
+            <div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
         )
     }
 }
 
-export default PostsId
+function mapStateToProps({posts}) {
+    return {posts}
+}
+
+export default connect(mapStateToProps,{fetchPosts})(PostsId)
--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -4,12 +4,13 @@ import * as serviceWorker from './serviceWorker';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
+import promise from 'redux-promise'
 
 import reducers from './reducers'
 import {BrowserRouter, Route} from "react-router-dom";
 import PostsId from "./components/postsId";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
